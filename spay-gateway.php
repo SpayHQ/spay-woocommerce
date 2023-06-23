052200581bc7c4271ce4c010961dda693dd483be
Fola Akinbobola <voachamber@gmail.com>
2023-06-23 17:02:46 +0100

    fix commit message typo

--- spay-gateway.php
+++ spay-gateway.php
@@ -6,8 +6,8 @@
 	Version: 1.0.1
 	Author: Data Quotient Limited
 	Author URI: http://dataquot.com
-  License:           GPL-2.0+
- 	License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+  License:         GPL-2.0+
+ 	License URI:     http://www.gnu.org/licenses/gpl-2.0.txt
  	GitHub Plugin URI: https://github.com/SpayHQ/spay-woocommerce
 */
 add_action( 'plugins_loaded', 'dq_spay_payments_init', 0 );
